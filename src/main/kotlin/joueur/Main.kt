3660e2af19091e20cfe8ca628a38e93defc84070
gege2108 <germain.potet@outlook.fr>
2026-01-18 15:09:04 +0100

    avro semble être fonctionnel. Il traduit bien AchatJeuEvent

--- src/main/kotlin/joueur/Main.kt
+++ src/main/kotlin/joueur/Main.kt
@@ -2,6 +2,8 @@ import model.Jeux
 import model.Joueur
 import service.Evenement
 import java.time.LocalDate
+import com.projet.joueur.AchatJeuEvent
+
 
 fun main() {
     val j = Joueur("Sniper99", "Dupont", "Jean", LocalDate.of(2000, 5, 15))
@@ -13,4 +15,18 @@ fun main() {
 
     j.mapTempsDeJeux[elden.nomJeux] = 2.5f // Simule le temps passé [cite: 50]
     engine.creerCommentaireJeu(elden)
+
+    try {
+        val event = AchatJeuEvent.newBuilder()
+            .setPseudo("German")           // Pos 0
+            .setNomJeu("Elden Ring")       // Pos 1
+            .setSupport("PS5")          // Pos 2 (C'est probablement celui-ci qui manque !)
+            .setPrixPaye(50)
+            .setTimestamp(System.currentTimeMillis())
+            .build()
+
+        println("✅ Succès Avro : Objet créé pour le joueur ${event.getPseudo()}")
+    } catch (e: Exception) {
+        println("❌ Erreur Avro : ${e.message}")
+    }
 }
